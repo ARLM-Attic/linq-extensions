5d7f54f3d2fe4911fc6ad8554d2d50fdff80b573
SND\tomasp_cp <SND\tomasp_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2007-09-22 17:24:47 +0000

    git-svn-id: https://linqextensions.svn.codeplex.com/svn@4610 2327b42d-5241-43d6-9e2a-de5ac946f064

--- LinqExt/Demos/Demos.Functional.Mandel/MainForm.cs
+++ LinqExt/Demos/Demos.Functional.Mandel/MainForm.cs
@@ -17,9 +17,13 @@ namespace Demos.Functional.Mandel
 			InitializeComponent();
 		}
 
-		double cx = -0.55, wid = 3.1;
-		double cy = 0, hgt = 2.6;
+		// Initial position in the set
+		float cx = -0.55f, wid = 3.1f;
+		float cy = 0f, hgt = 2.6f;
 
+		/// <summary>
+		/// Refresh the image
+		/// </summary>
 		protected override void OnResize(EventArgs e)
 		{
 			var res = this.BackgroundImage;
@@ -28,33 +32,32 @@ namespace Demos.Functional.Mandel
 			base.OnResize(e);
 		}
 
+		/// <summary>
+		/// Change zoom and position in the set
+		/// (left click for zoom-in, right for zoom-out)
+		/// </summary>
 		protected override void OnMouseDown(MouseEventArgs e)
 		{
 			cx = wid * (e.X - Width / 2) / Width + cx;
 			cy = hgt * (e.Y - Height / 2) / Height + cy;
 			if (e.Button == MouseButtons.Left)
 			{
-				wid = wid / 1.5;
-				hgt = hgt / 1.5;
+				wid = wid / 1.5f;
+				hgt = hgt / 1.5f;
 			}
 			else if (e.Button == MouseButtons.Right)
 			{
-				wid = wid * 1.5;
-				hgt = hgt * 1.5;
+				wid = wid * 1.5f;
+				hgt = hgt * 1.5f;
 			}
 			this.OnResize(EventArgs.Empty);
 			base.OnMouseDown(e);
 		}
 
-		private Bitmap InitBitmap_Safe(int width, int height, Func<int, int, Color> f)
-		{
-			Bitmap b = new Bitmap(width, height);
-			for (int x = 0; x < this.Width; x++)
-				for (int y = 0; y < this.Height; y++)
-					b.SetPixel(x, y, f(x,y));
-			return b;				
-		}
-
+		/// <summary>
+		/// Initialize bitmap of specified size using given function.
+		/// (Implemented using C# unsafe code)
+		/// </summary>
 		unsafe static private Bitmap InitBitmap(int width, int height, Func<int, int, Color> f)
 		{
 			Bitmap b = new Bitmap(width, height);
@@ -74,43 +77,75 @@ namespace Demos.Functional.Mandel
 			return b;
 		}
 
+		/// <summary>
+		/// Draw the Mandelbrot set
+		/// </summary>
 		private Bitmap DrawMandel()
 		{
+			// Generate palette by concatenating transition from black 
+			// to blue and from blue to green
 			var colors = 
 				Seq.Concat(
 					Seq.Range(0, 127).Select(n => Color.FromArgb(0, n * 2, 0)),
 					Seq.Range(0, 127).Select(n => Color.FromArgb(0, 255 - n * 2, n))).ToArray();
 
-
-			double dx = wid / this.Width;
-			double dy = hgt / this.Height;
-			double xmin = cx - wid / 2, ymin = cy - hgt / 2;
+			// Calculate width and center of the image
+			float dx = wid / this.Width;
+			float dy = hgt / this.Height;
+			float xmin = cx - wid / 2, ymin = cy - hgt / 2;
+			
 			return InitBitmap(this.Width, this.Height, (x, y) => {
-					int count = GenerateMandel(xmin + x * dx, ymin + y * dy).CountMax(100);
+					float xs = xmin + x * dx, ys = ymin + y * dy;
+
+					// Count number of iterations with maximum number 100
+					int count = GenerateMandel(xs, ys).CountMax(100);
+
+					// Get color depending on the number of iterations
 					int val = (int)((count / 100.0) * 255.0);
 					return colors[val];
 				});
 		}
 
-		private static IEnumerable<Tuple<double, double>> GenerateMandel(double x, double y)
+		/// <summary>
+		/// Generate the sequence for a pixel in the image..
+		/// </summary>
+		/// <returns></returns>
+		private static IEnumerable<PointF> GenerateMandel(float x, float y)
 		{
-			return Seq.Generate(Tuple.New(0.0, 0.0), (t) => {
-					double x2 = t.First * t.First, y2 = t.Second * t.Second;
+			var init = new PointF(0.0f, 0.0f);
+			return Seq.Generate(init, (t) => {
+					// Calculate squares of X and Y
+					float x2 = t.X * t.X, y2 = t.Y * t.Y;
+
+					// If the sum is out of range then return 'null'
 					if (Math.Sqrt(x2 + y2) >= 2)
 						return null; 
+					// Otherwise calculate the next point in the sequence
 					else 
-						return Tuple.New(x2 - y2 + x, 2 * t.First * t.Second + y);
+						return new PointF(x2 - y2 + x, 2 * t.X * t.Y + y);
 				});
 		}
 	}
 
+
+	/// <summary>
+	/// Class with utility functions for working with sequences
+	/// </summary>
 	public static class Seq
 	{
+		/// <summary>
+		/// Count number of iterations, but stop after specified maximal number.
+		/// </summary>
 		public static int CountMax<T>(this IEnumerable<T> en, int max)
 		{
-			return en.TakeWhile((_) => max-- > 0).Count();
+			return en.TakeWhile(_ => max-- > 0).Count();
 		}
 
+		/// <summary>
+		/// Generates a sequence inductively. The first elemente in the sequence is
+		/// given as a first argument and the next element is generated using the function
+		/// given as a second argument. When the function returns 'null' the sequence ends.
+		/// </summary>
 		public static IEnumerable<T> Generate<T>(T initial, Func<T, T?> next) where T : struct
 		{
 			T? val = initial;
@@ -121,23 +156,21 @@ namespace Demos.Functional.Mandel
 			}
 		}
 
-		public static IEnumerable<int> Integers()
-		{
-			int i = 0;
-			while (true) yield return i++;
-		}
-
+		/// <summary>
+		/// Generate sequence of integers in the specified range.
+		/// </summary>
 		public static IEnumerable<int> Range(int from, int to)
 		{
 			for (int i = from; i <= to; i++) yield return i;
 		}
 
+		/// <summary>
+		/// Concatenate any number of sequences given as arguments
+		/// </summary>
 		public static IEnumerable<T> Concat<T>(params IEnumerable<T>[] args)
 		{
 			foreach (var en in args)
 				foreach (var el in en) yield return el;
 		}
 	}
-
-
 }
---
+++ LinqExt/Demos/Demos.Infinite.Intro/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demos.Infinite.Intro
+{
+	class Program
+	{
+		/// <summary>
+		/// Returns an infinite sequence of integers: 0, 1, 2, 3, ...
+		/// </summary>
+		static IEnumerable<int> GetNumbers()
+		{
+			int i = 0;
+			while(true) yield return i++;
+		}
+
+		/// <summary>
+		/// Generates a sequence inductively. The first elemente in the sequence is
+		/// given as a first argument and the next element is generated using the function
+		/// given as a second argument. When the function returns 'null' the sequence ends.
+		/// </summary>
+		static IEnumerable<T> Generate<T>(T initial, Func<T, T?> next) 
+			where T : struct
+		{
+			T? val = initial;
+			while (val.HasValue)
+			{
+				yield return val.Value;
+				val = next(val.Value);
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			// Create an seq.: 0, 1, 2, 3, 4, ...
+			var nums = GetNumbers();
+
+			// Squares of even numbers: 0, 4, 16, 36, ...
+			var sq = GetNumbers().Where(n => n % 2 == 0).Select(n => n * n);
+			// Alternative - using LINQ query comprehension
+			var sq2 = from n in GetNumbers() where n % 2 == 0 select n * n;
+
+			Console.Write("Squares of even numbers: ");
+			foreach (var n in sq.Take(20))
+				Console.Write("{0}, ", n);
+
+
+			// Powers of two that fit into 
+			var pow = Generate(1, n => {
+					if (n > Int32.MaxValue / 2)
+						return null;
+					else
+						return (int?)(n * 2);
+				});
+			Console.Write("\n\nPowers of two that fit in 'int' type: ");
+			foreach (var n in pow)
+				Console.Write("{0}, ", n);			
+		}
+	}
+}
